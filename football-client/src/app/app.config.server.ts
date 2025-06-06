fb3b1fd27590927911181c3193659cb7c75bc4ab
dittko <wiktorm205@gmail.com>
2025-06-06 12:08:31 +0200

    fix5

--- football-client/src/app/app.config.server.ts
+++ football-client/src/app/app.config.server.ts
@@ -1,12 +1,10 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { serverRoutes } from './app.routes.server';
 export { config } from './app.config';
 
-const serverConfig: ApplicationConfig = {
+export const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(withRoutes(serverRoutes))
   ]
 };
-
-
